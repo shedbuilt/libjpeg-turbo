8ccd41d5c0d425abd667c275f93c3880f59f986f
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:15:44 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
             --with-jpeg8            \
             --disable-static        \
             --docdir=/usr/share/doc/libjpeg-turbo-1.5.3 && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install
